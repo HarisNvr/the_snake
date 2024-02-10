5a5532328a35b2b1c98a01e367ad9b0cf6b3e9f8
Haro Mikhaylov <www.haris@bk.ru>
2024-02-10 16:22:34 +0300

    Release 3D

--- the_snake_3D.py
+++ the_snake_3D.py
@@ -2,11 +2,13 @@ from random import choice
 
 import pygame as pg
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
-SCREEN_CENTER = ((SCREEN_WIDTH // 2), (SCREEN_HEIGHT // 2))
+PLAYABLE_SCREEN_WIDTH, PLAYABLE_SCREEN_HEIGHT = 640, 480
+PLAYABLE_SCREEN_CENTER = ((PLAYABLE_SCREEN_WIDTH // 2),
+                          (PLAYABLE_SCREEN_HEIGHT // 2))
 GRID_SIZE = 20
-GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
-GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+GRID_WIDTH = PLAYABLE_SCREEN_WIDTH // GRID_SIZE
+GRID_HEIGHT = PLAYABLE_SCREEN_HEIGHT // GRID_SIZE
+
 ALL_POSITIONS = [
     (x * GRID_SIZE, y * GRID_SIZE) for x in range(GRID_WIDTH) for
     y in range(GRID_HEIGHT)
@@ -27,8 +29,27 @@ SNAKE_BODY_COLOR = (0, 128, 0)
 SNAKE_HEAD_COLOR = (50, 205, 50)
 
 BASE_SPEED = 15
+SPEED_MODIFIERS = {
+    range(0, 6): 0,
+    range(6, 11): 1,
+    range(11, 21): 2,
+    range(21, 31): 3,
+    range(31, 41): 4,
+    range(41, 56): 5,
+    range(56, 76): 6,
+    range(76, 101): 7,
+    range(101, 131): 9,
+    range(131, 171): 11,
+    range(171, 221): 13,
+    range(221, 251): 14,
+    range(251, 301): 15
+}
 
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), depth=32)
+SCREEN = pg.display.set_mode((PLAYABLE_SCREEN_WIDTH,
+                              PLAYABLE_SCREEN_HEIGHT + 100), depth=32)
+
+SCREEN_FILLER = pg.image.load('screen_img.jpg').convert()
+SCOREBOARD_FILLER = pg.image.load('scoreboard_1.png').convert()
 
 pg.display.set_caption('Pythot by HarisNvr')
 
@@ -60,9 +81,15 @@ class GameObject:
 
     def __init__(self, body_color=None, border_color=None):
         """Parent class constructor"""
-        self.position = SCREEN_CENTER
+        self.position = PLAYABLE_SCREEN_CENTER
         self.body_color = body_color
         self.border_color = border_color
+
+    def draw_square(self, position, color, surface):
+        """Draw 20x20 square object"""
+        rect = pg.Rect(position, (GRID_SIZE, GRID_SIZE))
+        pg.draw.rect(surface, color, rect)
+        pg.draw.rect(surface, self.border_color, rect, 1)
 
     def draw(self, surface):
         """Preform method for Snake and Apple cls"""
@@ -73,8 +100,8 @@ class GameObject:
         """Displays 'YOU WIN' message"""
         font = pg.font.Font(None, 74)
         text = font.render("YOU WIN", True, (255, 255, 0))
-        text_rect = text.get_rect(center=SCREEN_CENTER)
-        screen.blit(text, text_rect)
+        text_rect = text.get_rect(center=PLAYABLE_SCREEN_CENTER)
+        SCREEN.blit(text, text_rect)
         pg.display.flip()
 
         end_time = pg.time.get_ticks() + 5 * 1000
@@ -103,7 +130,7 @@ class Apple(GameObject):
     def randomize_position(self, occupied_positions=None):
         """Change current object position into random non-occupied value"""
         if occupied_positions is None:
-            occupied_positions = [SCREEN_CENTER]
+            occupied_positions = [PLAYABLE_SCREEN_CENTER]
 
         free_pos = self.available_positions(occupied_positions)
 
@@ -112,12 +139,7 @@ class Apple(GameObject):
 
     def draw(self, surface):
         """Draw apple cls object"""
-        rect = pg.Rect(
-            (self.position[0], self.position[1]),
-            (GRID_SIZE, GRID_SIZE)
-        )
-        pg.draw.rect(surface, self.body_color, rect)
-        pg.draw.rect(surface, self.border_color, rect, 1)
+        self.draw_square(self.position, self.body_color, surface)
 
 
 class Snake(GameObject):
@@ -126,11 +148,11 @@ class Snake(GameObject):
     def __init__(self, body_color=SNAKE_BODY_COLOR, border_color=BORDER_COLOR):
         """Snake class constructor"""
         super().__init__(body_color, border_color)
+        self.head_color = SNAKE_HEAD_COLOR
         self.length = 1
         self.positions = [self.position]
-        self.direction = RIGHT
+        self.direction = choice([LEFT, RIGHT, UP, DOWN])
         self.next_direction = None
-        self.head_color = SNAKE_HEAD_COLOR
 
     def update_direction(self):
         """Replace current direction value with new direction value"""
@@ -144,24 +166,26 @@ class Snake(GameObject):
         and pop the last value
         """
         snake_x, snake_y = self.get_head_position()
-        new_snake_x = (snake_x + self.direction[0] * GRID_SIZE) % SCREEN_WIDTH
-        new_snake_y = (snake_y + self.direction[1] * GRID_SIZE) % SCREEN_HEIGHT
+        new_snake_x = ((snake_x + self.direction[0] * GRID_SIZE) %
+                       PLAYABLE_SCREEN_WIDTH)
+        new_snake_y = ((snake_y + self.direction[1] * GRID_SIZE) %
+                       PLAYABLE_SCREEN_HEIGHT)
         self.positions.insert(0, (new_snake_x, new_snake_y))
         if len(self.positions) > self.length:
             self.positions.pop()
 
-    def draw(self, surface):
-        """Draw head and body of the snake cls object"""
-        for position in self.positions:
-            rect = (
-                pg.Rect((position[0], position[1]), (GRID_SIZE, GRID_SIZE))
-            )
-            pg.draw.rect(surface, self.body_color, rect)
-            pg.draw.rect(surface, self.border_color, rect, 1)
+    def draw_snake_head(self, surface):
+        """Draw snake's head"""
+        self.draw_square(self.positions[0], self.head_color, surface)
 
-        head_rect = pg.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
-        pg.draw.rect(surface, self.head_color, head_rect)
-        pg.draw.rect(surface, self.border_color, head_rect, 1)
+    def draw_full_snake(self, surface):
+        """Draw body of the snake and add it's head to it"""
+        if len(self.positions) != 1:
+            for position in self.positions[1:]:
+                self.draw_square(position, self.body_color, surface)
+            self.draw_snake_head(surface)
+        else:
+            self.draw_snake_head(surface)
 
     def get_head_position(self):
         """Return value of the first element in positions list"""
@@ -180,13 +204,19 @@ class Snake(GameObject):
 def main():
     """Game body"""
     pg.init()
+    pg.font.init()
     clock.tick(BASE_SPEED)
 
     apple = Apple()
     snake = Snake()
 
+    score_font = pg.font.Font('Lordcorps[RUSbyPenka220]-Stencil.ttf', 36)
+
     while True:
-        actual_speed = BASE_SPEED + (snake.length // 5)
+        actual_speed = BASE_SPEED + SPEED_MODIFIERS[
+            next(k for k in SPEED_MODIFIERS.keys() if snake.length in k)
+        ]
+
         clock.tick(actual_speed)
 
         handle_keys(snake)
@@ -200,14 +230,23 @@ def main():
             snake.reset()
 
         snake.update_direction()
-        screen.fill(BOARD_BACKGROUND_COLOR)
 
-        snake.draw(screen)
+        score = score_font.render(f'Length: {snake.length}', True,
+                                  (255, 255, 255))
+        speed = score_font.render(f'Speed: {actual_speed}', True,
+                                  (255, 255, 255))
+
+        SCREEN.blit(SCREEN_FILLER, (0, 0))
+        SCREEN.blit(SCOREBOARD_FILLER, (0, 480))
+        SCREEN.blit(score, (25, 507))
+        SCREEN.blit(speed, (450, 507))
+
+        snake.draw_full_snake(SCREEN)
 
         if not apple.available_positions(snake.positions):
             GameObject.display_win_message()
 
-        apple.draw(screen)
+        apple.draw(SCREEN)
 
         pg.display.update()
 
